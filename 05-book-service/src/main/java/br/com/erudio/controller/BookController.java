2f62aae2791e4ea51e7d886ee61a5cf4dd098a69
Heberty Guelfi do Prado <hebertyprado22005@gmail.com>
2022-12-26 15:54:59 -0300

    Section Completion - Creating and Connecting Microservices with Feign

--- 05-book-service/src/main/java/br/com/erudio/controller/BookController.java
+++ 05-book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -1,18 +1,15 @@
 package br.com.erudio.controller;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.erudio.model.Book;
+import br.com.erudio.proxy.CambioProxy;
 import br.com.erudio.repository.BookRepository;
-import br.com.erudio.response.Cambio;
 
 @RestController
 @RequestMapping("book-service")
@@ -24,6 +21,9 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
+	@Autowired
+	private CambioProxy proxy;
+	
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(
 			@PathVariable("id") Long id,
@@ -33,21 +33,10 @@ public class BookController {
 		var book = repository.findById(id).get();
 		if(book == null) throw new RuntimeException("Book not Found");
 		
-		HashMap<String, String> params = new HashMap<>();
-		params.put("amount", book.getPrice().toString());
-		params.put("from", "USD");
-		params.put("to", currency);
-		
-		var response = new RestTemplate()
-			.getForEntity("http://localhost:8000/cambio-service/"
-					+ "{amount}/{from}/{to}",
-					Cambio.class,
-					params);
-		
-		var cambio = response.getBody();
+		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 		
 		var port = environment.getProperty("local.server.port");
-		book.setEnvironment(port);
+		book.setEnvironment(port + " FEIGN");
 		book.setPrice(cambio.getConvertedValue());
 		return book;
 	}
