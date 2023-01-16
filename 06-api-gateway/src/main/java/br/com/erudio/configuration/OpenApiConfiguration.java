9de10c39ce8daf954129a3d0e6fece53c63e8216
Heberty Guelfi do Prado <hebertyprado22005@gmail.com>
2023-01-16 12:27:32 -0300

    Adjusting Settings to Render Swagger UI

--- 06-api-gateway/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
+++ 06-api-gateway/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
@@ -1,13 +1,18 @@
 package br.com.erudio.configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class OpenApiConfiguration {
 	
+	@Bean
+	@Lazy(false)
 	public List<GroupedOpenApi> apis(SwaggerUiConfigParameters config,
 			RouteDefinitionLocator locator) {
 		
